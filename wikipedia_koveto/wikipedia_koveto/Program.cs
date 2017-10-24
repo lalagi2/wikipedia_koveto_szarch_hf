6207e43d82f4aa4f1560512b8d6e30f3c3b9d58e
Kinyik Bence <kinyik94@gmail.com>
2017-10-25 00:06:14 +0200

    Add Database to app
     * Entity Framework for easy access to db

--- wikipedia_koveto/wikipedia_koveto/Program.cs
+++ wikipedia_koveto/wikipedia_koveto/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using wikipedia_koveto.Model;
 
 namespace wikipedia_koveto
 {
@@ -14,12 +16,36 @@ namespace wikipedia_koveto
         [STAThread]
         static void Main()
         {
-            EmailSender emailSender = new EmailSender();
-            emailSender.sendEmail("tothlajosg@gmail.com", "toth lajos", "subject", "uzenet");
+            // Test email
+            //EmailSender emailSender = new EmailSender();
+            //emailSender.sendEmail("tothlajosg@gmail.com", "toth lajos", "subject", "uzenet");
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new LoginForm());
+
+            // Test Database
+            using (UserDataEntities dc = new UserDataEntities())
+            {
+                var users = from user in dc.Users select new { user.UserName, user.Email };
+                foreach (var user in users)
+                {
+                    Console.WriteLine("( " + user.UserName + ", " + user.Email + " )");
+                }
+
+                var groups = from page in dc.Pages
+                            group page by page.UserName into g
+                            select g;
+                foreach (var g in groups)
+                {
+                    Console.WriteLine(g.Key);
+                    foreach (var page in g)
+                    {
+                        Console.WriteLine("   {0}, {1}, {2}", page.WikiPage, page.Sensitivity, page.NotificationNumber);
+                    }
+                }
+
+            }
         }
     }
 }
